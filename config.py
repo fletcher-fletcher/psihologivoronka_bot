a75b15d41bd96ccc2b193e112d347b567ac740b7
fletcher-fletcher <translservicio@gmail.com>
2026-03-13 11:56:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-BOT_TOKEN = "8722694608:AAHeFMhLVDbdpoAn0-aOpSyABq92rFZod3E"
+BOT_TOKEN = "8601276030:AAHrcXdbWcSDldgi0aNavnvSKH7exyNlP5U"
 
 # Ссылка на запись
 BOOKING_URL = "https://vmarte.life/#rec575375733"
 
 # ID администратора (твой Telegram ID)
+
 ADMIN_ID = 5333876901  # Узнай у @userinfobot
